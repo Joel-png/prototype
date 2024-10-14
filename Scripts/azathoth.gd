de967b770883550fe938086884e4802d5674445f
Joel-png <joel.d.woodhouse@gmail.com>
2024-10-15 04:26:58 +1100

    big eye

--- Scripts/azathoth.gd
+++ Scripts/azathoth.gd
@@ -13,35 +13,15 @@ func _process(delta):
 		look_at_position(closets_player_position, delta)
 	
 
-func look_at_position(pos, delta):
+func look_at_position(target_position, delta):
 	var rotation_speed = 2.0
-	var target_position = pos
 	var direction = (target_position - eye.global_position)
 	var direction_x_z = pow(pow(direction.x, 2) + pow(direction.z, 2), 0.5)
 	var angle_pitch = atan2(direction_x_z, direction.y) + PI/2.0
 	var angle_yaw = -atan2(direction.z, direction.x) + PI/2.0
 	
-	#fix_rotation(eye.rotation.y, angle_yaw)
 	eye.rotation.y = lerp_angle(eye.rotation.y, angle_yaw, rotation_speed * delta)
 	eye.rotation.x = lerp_angle(eye.rotation.x, angle_pitch, rotation_speed * delta)
-	
-	
-	
-func fix_rotation(rot_from, rot_to):
-	counter += 1
-	if counter > 1000:
-		print("NOT")
-		print(rot_from)
-		print(rot_to)
-		counter = 0
-	if eye.rotation.y > 0 and rot_to < 0:
-		print(rot_from)
-		print(rot_to)
-		eye.rotation.y -= deg_to_rad(360)
-	elif eye.rotation.y < 0 and rot_to > 0:
-		print(rot_from)
-		print(rot_to)
-		eye.rotation.y += deg_to_rad(360)
 
 func get_closest_player_index(players):
 	var overlapping_bodies = players
