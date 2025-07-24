58a958bdfe926a8aefe4f91355b98126d6896a33
Joel-png <joel.d.woodhouse@gmail.com>
2025-07-25 07:25:18 +1000

    better fireball collision

--- Scripts/fireball.gd
+++ Scripts/fireball.gd
@@ -1,9 +1,15 @@
 extends Projectile
 
-var projectile_speed = 10.0
+var projectile_speed = 10000.0
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	position += transform.basis * Vector3(0, 0, -projectile_speed) * delta
+	if get_slide_collision_count() > 0:
+		print("collided")
+		self.queue_free()
+	velocity = transform.basis * Vector3(0, 0, -projectile_speed)
+	move_and_slide()
+	
+	
