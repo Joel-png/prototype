7518a3884fd8f69e2d515e325fbc064277eb0191
Joel-png <joel.d.woodhouse@gmail.com>
2025-08-07 06:45:22 +1000

    this is bad

--- enemy_spawner.gd
+++ enemy_spawner.gd
@@ -1,36 +1,43 @@
 extends Node3D
 
 # tokens dictate the bias of spawn weights
-var spawn_tokens = 100
+var spawn_tokens = 200
 var enemy_spawn_weights: Array[float]
 var enemy_data = {
-	"scuttler": 5,
+	"scuttler": 25,
 	"azathoth": 100
 }
 var enemy_cost: Array[int]
 
 func _ready() -> void:
-	enemy_spawn_weights.resize(enemy_data.size())
 	for key in enemy_data.keys():
 		enemy_cost.append(enemy_data[key])
 	
 	set_weights()
+	set_weights()
+	set_weights()
+	set_weights()
+	set_weights()
 
 func set_weights():
+	enemy_spawn_weights.resize(enemy_data.size())
+	for i in range(0, enemy_spawn_weights.size()):
+		enemy_spawn_weights[i] = 0
 	var total_tokens = spawn_tokens
-	var tokens_left = spawn_tokens
+	var tokens_left = spawn_tokens * randf_range(0.5, 2.0)
 	var enemy_set: Array[bool]
 	for i in range(0, enemy_data.size()):
 		enemy_set.append(false)
 	
-	for i in range(enemy_cost.size() - 1, -1, -1):
-		if not enemy_set[i]:
-			var current_enemy_cost = enemy_cost[i]
-			if tokens_left >= current_enemy_cost:
-				var weight_from_tokens = total_tokens/current_enemy_cost
-				enemy_spawn_weights[i] += weight_from_tokens
-				tokens_left -= weight_from_tokens
-				enemy_set[i] = true
+	#while tokens_left > 25:
+	for i in range(0, 5):
+		var j = randi_range(0, enemy_cost.size() - 1)
+		var current_enemy_cost = enemy_cost[j]
+		if current_enemy_cost <= spawn_tokens:
+			var weight_from_tokens = ceil(max(0.1, current_enemy_cost/total_tokens * randf_range(0.5, 10.0)))
+			enemy_spawn_weights[j] += weight_from_tokens
+	#fix this
+	
 	
 	print("enemy spawner stuff")
 	print(tokens_left)
